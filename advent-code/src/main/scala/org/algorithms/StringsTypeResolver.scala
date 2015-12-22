d9d807f05a09138de74b9a21f8ad75338257b41e
Alex-Diez <alexvic.amatory@gmail.com>
2015-12-22 06:57:08 +0000

    nice string fifth day

--- advent-code/src/main/scala/org/algorithms/StringsTypeResolver.scala
+++ advent-code/src/main/scala/org/algorithms/StringsTypeResolver.scala
@@ -1,7 +1,39 @@
 package org.algorithms
 
+import scala.io.Source._
+
 class StringsTypeResolver {
     def isNice(string: String): Boolean = {
-        true
+        if (!string.contains("ab") && !string.contains("cd") && !string.contains("pq") && !string.contains("xy")) {
+            var previous = ' '
+            var vowels = List[Char]()
+            var hasDoubles = false
+            val vowelsSet = Set('a', 'e', 'i', 'o', 'u')
+            val iter = string.iterator
+            while (iter.hasNext) {
+                val c = iter.next
+                if (!hasDoubles && c == previous) {
+                    hasDoubles = true
+                }
+                if (vowelsSet.contains(c)) {
+                    vowels = c :: vowels
+                }
+                previous = c
+            }
+            hasDoubles && vowels.size > 2
+        }
+        else {
+            false
+        }
+    }
+}
+
+object StringsTypeResolver {
+
+    def main (args: Array[String]) {
+        val str = new StringsTypeResolver
+        val data = fromFile("advent-code/src/main/resources/strings")
+                .getLines().count(s => str.isNice(s))
+        println(data)
     }
 }
