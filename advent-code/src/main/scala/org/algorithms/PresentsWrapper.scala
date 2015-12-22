2788f7bf650418341b51e46f75b759510d2a703e
Alex-Diez <alexvic.amatory@gmail.com>
2015-12-22 12:53:34 +0000

    ribbon length second part second day

--- advent-code/src/main/scala/org/algorithms/PresentsWrapper.scala
+++ advent-code/src/main/scala/org/algorithms/PresentsWrapper.scala
@@ -5,9 +5,6 @@ import scala.language.postfixOps
 
 class PresentsWrapper() {
     def squareFor(size: String): Int = {
-        def evalEdges(size: String): List[Int] = {
-            size split "x" map[Int, Array[Int]] (s => s toInt) toList
-        }
         def createSides(edges: List[Int], sides: List[Int]): List[Int] = {
             if(edges.isEmpty) sides
             else edges.tail.map[Int, List[Int]](v => v * edges.head) ::: createSides(edges.tail, sides)
@@ -17,6 +14,17 @@ class PresentsWrapper() {
         }
         squareFor(createSides(evalEdges(size), Nil))
     }
+
+    def ribbonLength(size: String): Int = {
+        val edges = evalEdges(size)
+        val iMax = edges.indexOf(edges.max)
+        val minEdges = edges.patch(iMax, List(), 1)
+        2 * minEdges.sum + edges.product
+    }
+
+    private def evalEdges(size: String): List[Int] = {
+        size split "x" map[Int, Array[Int]] (s => s toInt) toList
+    }
 }
 
 object PresentsWrapper {
@@ -24,8 +32,9 @@ object PresentsWrapper {
 
     def main(args: Array[String]): Unit = {
         val pw = PresentsWrapper()
-        val data = fromFile("advent-code/src/main/resources/boxes")
-                .getLines().fold[String]("0")((acc: String, v: String) => (pw.squareFor(v) + acc.toInt).toString)
-        println(data)
+        val linesOne = fromFile("advent-code/src/main/resources/boxes").getLines
+        println(linesOne.map((v: String) => pw.squareFor(v)).sum)
+        val linesTwo = fromFile("advent-code/src/main/resources/boxes").getLines
+        println(linesTwo.map((v: String) => pw.ribbonLength(v)).sum)
     }
 }
--- advent-code/src/test/scala/org/algorithms/PresentsWrapperTest.scala
+++ advent-code/src/test/scala/org/algorithms/PresentsWrapperTest.scala
@@ -1,21 +1,32 @@
 package org.algorithms
 
-import org.scalatest.{BeforeAndAfter, Matchers, FunSuite}
+import org.scalatest.{FunSuite, Matchers}
 
 class PresentsWrapperTest extends FunSuite with Matchers {
 
     val pw = PresentsWrapper()
 
     test("calculate paper for 1x1x1 box") {
-        pw.squareFor("1x1x1") shouldBe 7
+        pw.squareFor("1x1x1") shouldBe 2 * (1 * 1) + 2 * (1 * 1) + 2 * (1 * 1) + 1
+    }
+
+    test("calculate ribbon length for 1x1x1 box") {
+        pw.ribbonLength("1x1x1") shouldBe 2 * (1 + 1) + (1 * 1 * 1)
     }
 
     test("calculate paper for 2x1x1 box") {
-        pw.squareFor("2x1x1") shouldBe 11
+        pw.squareFor("2x1x1") shouldBe 2 * (1 * 1) + 2 * (2 * 1) + 2 * (2 * 1) + 1
+    }
+
+    test("calculate ribbon length for 2x1x1 box") {
+        pw.ribbonLength("2x1x1") shouldBe 2 * (1 + 1) + (2 * 1 * 1)
     }
 
     test("site tests :)") {
-        pw.squareFor("2x3x4") shouldBe 58
-        pw.squareFor("1x1x10") shouldBe 43
+        pw.squareFor("2x3x4") shouldBe 2 * (2 * 3) + 2 * (3 * 4) + 2 * (2 * 4) + 2 * 3
+        pw.squareFor("1x1x10") shouldBe 2 * (1 * 1) + 2 * (1 * 10) + 2 * (10 * 1) + 1 * 1
+
+        pw.ribbonLength("2x3x4") shouldBe 2 * (2 + 3) + (2 * 3 * 4)
+        pw.ribbonLength("1x1x10") shouldBe 2 * (1 + 1) + (1 * 1 * 10)
     }
 }
