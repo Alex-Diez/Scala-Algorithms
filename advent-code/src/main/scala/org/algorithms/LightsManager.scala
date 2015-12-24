8a1d597912ffb06fb3203f5694f6b76a68bb05c9
Alex-Diez <alexvic.amatory@gmail.com>
2015-12-24 09:04:40 +0000

    lights sixth day part two

--- advent-code/src/main/scala/org/algorithms/LightsManager.scala
+++ advent-code/src/main/scala/org/algorithms/LightsManager.scala
@@ -24,29 +24,61 @@ class LightsManager(rows: Int, cols: Int) {
     def toggle(xBegin: Int, yBegin: Int, xEnd: Int, yEnd: Int) = {
         forRectangle(xBegin, yBegin, xEnd, yEnd, (b) => if(b == 0) 1 else 0)
     }
+
+    def turnBrightOn(xBegin: Int, yBegin: Int, xEnd: Int, yEnd: Int) = {
+        forRectangle(xBegin, yBegin, xEnd, yEnd, (b) => b + 1)
+    }
+
+    def turnBrightOff(xBegin: Int, yBegin: Int, xEnd: Int, yEnd: Int) = {
+        forRectangle(xBegin, yBegin, xEnd, yEnd, (b) => if (b > 0) b - 1 else 0)
+    }
+
+    def toggleBright(xBegin: Int, yBegin: Int, xEnd: Int, yEnd: Int) = {
+        forRectangle(xBegin, yBegin, xEnd, yEnd, (b) => b + 2)
+    }
 }
 
 object LightsManager {
     def apply(rows: Int, cols: Int) = new LightsManager(rows, cols)
 
     def main(args: Array[String]) = {
-        val lm = LightsManager(1000, 1000)
-        fromFile("advent-code/src/main/resources/lights").getLines().foreach(s => {
-            val parts = s.split(" ")
-            val command = parts(0)
-            val datas = parts(1).split(",").map(s => s toInt)
-            if(command.equals("turnOn")) lm.turnOn(datas(0), datas(1), datas(2), datas(3))
-            else if(command.equals("turnOff")) lm.turnOff(datas(0), datas(1), datas(2), datas(3))
-            else lm.toggle(datas(0), datas(1), datas(2), datas(3))
-        })
-        var count = 0
-        for(i <- lm.lights.indices) {
-            for(j <- lm.lights(i).indices) {
-                if(lm.lights(i)(j) != 0) {
-                    count += 1
+        {
+            val lm = LightsManager(1000, 1000)
+            fromFile("advent-code/src/main/resources/lights").getLines().foreach(s => {
+                val parts = s.split(" ")
+                val command = parts(0)
+                val datas = parts(1).split(",").map(s => s toInt)
+                if(command.equals("turnOn")) lm.turnOn(datas(0), datas(1), datas(2), datas(3))
+                else if(command.equals("turnOff")) lm.turnOff(datas(0), datas(1), datas(2), datas(3))
+                else lm.toggle(datas(0), datas(1), datas(2), datas(3))
+            })
+            var count = 0
+            for(i <- lm.lights.indices) {
+                for(j <- lm.lights(i).indices) {
+                    if(lm.lights(i)(j) != 0) {
+                        count += 1
+                    }
                 }
             }
+            println(count)
         }
-        println(count)
+        {
+            val lm = LightsManager(1000, 1000)
+            fromFile("advent-code/src/main/resources/lights").getLines().foreach(s => {
+                val parts = s.split(" ")
+                val command = parts(0)
+                val datas = parts(1).split(",").map(s => s toInt)
+                if(command.equals("turnOn")) lm.turnBrightOn(datas(0), datas(1), datas(2), datas(3))
+                else if(command.equals("turnOff")) lm.turnBrightOff(datas(0), datas(1), datas(2), datas(3))
+                else lm.toggleBright(datas(0), datas(1), datas(2), datas(3))
+            })
+            var count = 0
+            for(i <- lm.lights.indices) {
+                for(j <- lm.lights(i).indices) {
+                    count += lm.lights(i)(j)
+                }
+            }
+            println(count)
+        }
     }
 }
