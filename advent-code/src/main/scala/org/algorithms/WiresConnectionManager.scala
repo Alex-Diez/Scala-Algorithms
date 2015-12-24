c60f2dd68f95db66d03d5f3468c332fc60612eee
Alex-Diez <alexvic.amatory@gmail.com>
2015-12-24 14:37:19 +0000

    limit numbers by 65535

--- advent-code/src/main/scala/org/algorithms/WiresConnectionManager.scala
+++ advent-code/src/main/scala/org/algorithms/WiresConnectionManager.scala
@@ -1,30 +1,34 @@
 package org.algorithms
 
+import scala.io.Source._
+
 class WiresConnectionManager {
-    var signals = Map[String, () => Short]()
+    val ops = Set("AND", "OR", "NOT", "LSHIFT", "RSHIFT")
+    val digs = '0' until '9' toSet
+    var signals = Map[String, () => Int]()
 
-    private def defaultOperation(): () => Short = () => -1
+    private def defaultOperation(): () => Int = () => -1
 
-    private def noOperationFunction(value: Short): () => Short = () => value
+    private def noOperationFunction(value: Int): () => Int = () => value
 
-    private def andOperationOnWires(w1: String, w2: String): () => Short =
-        () => (execWireFunc(w1) & execWireFunc(w2)).toShort
+    private def andOperationOnWires(w1: String, w2: String): () => Int =
+        () => ((execWireFunc(w1) & execWireFunc(w2)) << 16) >> 16
 
-    private def orOperationOnWires(w1: String, w2: String): () => Short =
-        () => (execWireFunc(w1) | execWireFunc(w2)).toShort
+    private def orOperationOnWires(w1: String, w2: String): () => Int =
+        () => ((execWireFunc(w1) | execWireFunc(w2)) << 16) >> 16
 
-    private def lShiftOnWires(w1: String, shiftFactor: Short): () => Short =
-        () => (execWireFunc(w1) << shiftFactor).toShort
+    private def lShiftOnWires(w1: String, shiftFactor: Int): () => Int =
+        () => ((execWireFunc(w1) << shiftFactor) << 16) >> 16
 
-    private def rShiftOnWires(w1: String, shiftFactor: Short): () => Short =
-        () => (execWireFunc(w1) >> shiftFactor).toShort
+    private def rShiftOnWires(w1: String, shiftFactor: Int): () => Int =
+        () => ((execWireFunc(w1) >> shiftFactor) << 16) >> 16
 
-    private def notOperationOnWires(wire: String): () => Short =
-        () => (~execWireFunc(wire)).toShort
+    private def notOperationOnWires(wire: String): () => Int =
+        () =>  ~execWireFunc(wire) & 65535
 
-    private def execWireFunc(wire: String): Short = signals.getOrElse(wire, defaultOperation())()
+    private def execWireFunc(wire: String): Int = signals.getOrElse(wire, defaultOperation())()
 
-    def connect(string: String) = {
+    private def connect(string: String) = {
         val parts = string.split(" -> ")
         if (parts(0).startsWith("NOT")) {
             val operations = parts(0).split(" ")
@@ -35,16 +39,32 @@ class WiresConnectionManager {
             operations(1) match {
                 case "AND"    => signals = signals + (parts(1) -> andOperationOnWires(operations(0), operations(2)))
                 case "OR"     => signals = signals + (parts(1) -> orOperationOnWires(operations(0), operations(2)))
-                case "LSHIFT" => signals = signals + (parts(1) -> lShiftOnWires(operations(0), operations(2) toShort))
-                case "RSHIFT" => signals = signals + (parts(1) -> rShiftOnWires(operations(0), operations(2) toShort))
+                case "LSHIFT" => signals = signals + (parts(1) -> lShiftOnWires(operations(0), operations(2) toInt))
+                case "RSHIFT" => signals = signals + (parts(1) -> rShiftOnWires(operations(0), operations(2) toInt))
             }
         }
     }
 
-    def input(string: String) = {
+    private def addSource(string: String) = {
         val parts = string.split(" -> ")
-        signals = signals + (parts(1) -> noOperationFunction(parts(0).toShort))
+        if (digs.forall(c => parts.contains(c))) signals = signals + (parts(1) -> noOperationFunction(parts(0).toInt))
+        else signals = signals + (parts(1) -> signals.getOrElse(parts(0), defaultOperation()))
     }
 
-    def signalOn(wire: String): Short = execWireFunc(wire).toShort
+    def input(string: String) = {
+        println(string)
+        if (ops.forall(op => !string.contains(op))) addSource(string)
+        else connect(string)
+    }
+
+    def signalOn(wire: String): Int = execWireFunc(wire)
+}
+
+object WiresConnectionManager {
+
+    def main(args: Array[String]) = {
+        val wcm = new WiresConnectionManager
+        fromFile("advent-code/src/main/resources/bitwise").getLines().foreach(l => wcm.input(l))
+        println(wcm.signalOn("a"))
+    }
 }
--- advent-code/src/test/scala/org/algorithms/WiresConnectionManagerTest.scala
+++ advent-code/src/test/scala/org/algorithms/WiresConnectionManagerTest.scala
@@ -9,64 +9,64 @@ class WiresConnectionManagerTest extends FunSuite with Matchers {
     test("provide signal on wires") {
         wcm.input("23 -> x")
 
-        wcm.signalOn("x") shouldBe 23
+        wcm.signalOn("x") shouldBe (23 & 65535)
 
         wcm.input("43 -> y")
 
-        wcm.signalOn("y") shouldBe 43
+        wcm.signalOn("y") shouldBe (43 & 65535)
     }
 
     test("provide AND computation of signal") {
         wcm.input("23 -> x")
         wcm.input("46 -> y")
 
-        wcm.connect("x AND y -> z")
+        wcm.input("x AND y -> z")
 
-        wcm.signalOn("z") shouldBe 23 & 46
+        wcm.signalOn("z") shouldBe (23 & 46 & 65535)
     }
 
     test("provide OR computation of signal") {
         wcm.input("23 -> x")
         wcm.input("46 -> y")
 
-        wcm.connect("x OR y -> z")
+        wcm.input("x OR y -> z")
 
-        wcm.signalOn("z") shouldBe 23 | 46
+        wcm.signalOn("z") shouldBe (23 | 46 & 65535)
     }
 
     test("provide LSHIFT computation of signal") {
         wcm.input("23 -> x")
 
-        wcm.connect("x LSHIFT 2 -> z")
+        wcm.input("x LSHIFT 2 -> z")
 
-        wcm.signalOn("z") shouldBe 23 << 2
+        wcm.signalOn("z") shouldBe (23 << 2 & 65535)
     }
 
     test("provide RSHIFT computation of signal") {
         wcm.input("23 -> x")
 
-        wcm.connect("x RSHIFT 2 -> z")
+        wcm.input("x RSHIFT 2 -> z")
 
-        wcm.signalOn("z") shouldBe 23 >> 2
+        wcm.signalOn("z") shouldBe (23 >> 2 & 65535)
     }
 
     test("provide NOT computation of signal") {
         wcm.input("23 -> x")
 
-        wcm.connect("NOT x -> z")
+        wcm.input("NOT x -> z")
 
-        wcm.signalOn("z") shouldBe ~23
+        wcm.signalOn("z") shouldBe (~23 & 65535)
     }
 
     test("complex computation") {
         wcm.input("123 -> x")
         wcm.input("456 -> y")
-        wcm.connect("x AND y -> d")
-        wcm.connect("x OR y -> e")
-        wcm.connect("x LSHIFT 2 -> f")
-        wcm.connect("y RSHIFT 2 -> g")
-        wcm.connect("NOT x -> h")
-        wcm.connect("NOT y -> i")
+        wcm.input("x AND y -> d")
+        wcm.input("x OR y -> e")
+        wcm.input("x LSHIFT 2 -> f")
+        wcm.input("y RSHIFT 2 -> g")
+        wcm.input("NOT x -> h")
+        wcm.input("NOT y -> i")
 
         wcm.signalOn("d") shouldBe 72
         wcm.signalOn("e") shouldBe 507
