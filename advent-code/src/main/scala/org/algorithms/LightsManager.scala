a5c3274bc27b0e8be789b8a8af9b4a4a30bb2eb8
Alex-Diez <alexvic.amatory@gmail.com>
2015-12-24 09:01:18 +0000

    lights sixth day part one

--- advent-code/src/main/scala/org/algorithms/LightsManager.scala
+++ advent-code/src/main/scala/org/algorithms/LightsManager.scala
@@ -1,13 +1,52 @@
 package org.algorithms
 
-class LightsManager {
-    val lights = Array.ofDim[Int](1000, 1000)
+import scala.io.Source._
 
-    def turnOn(xBegin: Int, yBegin:Int, xEnd:Int, yEnd:Int) = {
-        for(i <- yBegin until yEnd) {
-            for (j <- xBegin until  xEnd) {
-                lights(i)(j) = 1
+class LightsManager(rows: Int, cols: Int) {
+    val lights = Array.ofDim[Int](rows, cols)
+
+    private def forRectangle(xBegin: Int, yBegin: Int, xEnd: Int, yEnd: Int, func: (Int) => Int) = {
+        for(i <- xBegin to xEnd) {
+            for(j <- yBegin to yEnd) {
+                lights(i)(j) = func(lights(i)(j))
             }
         }
+    }
+
+    def turnOn(xBegin: Int, yBegin: Int, xEnd: Int, yEnd: Int) = {
+        forRectangle(xBegin, yBegin, xEnd, yEnd, (b) => 1)
+    }
+
+    def turnOff(xBegin: Int, yBegin: Int, xEnd: Int, yEnd: Int) = {
+        forRectangle(xBegin, yBegin, xEnd, yEnd, (b) => 0)
+    }
+
+    def toggle(xBegin: Int, yBegin: Int, xEnd: Int, yEnd: Int) = {
+        forRectangle(xBegin, yBegin, xEnd, yEnd, (b) => if(b == 0) 1 else 0)
+    }
+}
+
+object LightsManager {
+    def apply(rows: Int, cols: Int) = new LightsManager(rows, cols)
+
+    def main(args: Array[String]) = {
+        val lm = LightsManager(1000, 1000)
+        fromFile("advent-code/src/main/resources/lights").getLines().foreach(s => {
+            val parts = s.split(" ")
+            val command = parts(0)
+            val datas = parts(1).split(",").map(s => s toInt)
+            if(command.equals("turnOn")) lm.turnOn(datas(0), datas(1), datas(2), datas(3))
+            else if(command.equals("turnOff")) lm.turnOff(datas(0), datas(1), datas(2), datas(3))
+            else lm.toggle(datas(0), datas(1), datas(2), datas(3))
+        })
+        var count = 0
+        for(i <- lm.lights.indices) {
+            for(j <- lm.lights(i).indices) {
+                if(lm.lights(i)(j) != 0) {
+                    count += 1
+                }
+            }
+        }
+        println(count)
     }
 }
--- advent-code/src/test/scala/org/algorithms/LightsManagerTest.scala
+++ advent-code/src/test/scala/org/algorithms/LightsManagerTest.scala
@@ -1,17 +1,56 @@
 package org.algorithms
 
-import org.scalatest.{Matchers, FunSuite}
+import org.scalatest.{BeforeAndAfter, FunSuite, Matchers}
 
-class LightsManagerTest extends FunSuite with Matchers {
+class LightsManagerTest extends FunSuite with Matchers with BeforeAndAfter {
 
-    test("it should create a light manager") {
-        new LightsManager
+    var lm5x5 = LightsManager(5, 5)
+
+    before {
+        lm5x5 = LightsManager(5, 5)
     }
 
-    test("it should be light through 0, 0 to 999, 999") {
-        val lm = new LightsManager
-        lm.turnOn(0, 0, 999, 0)
+    test("it should be light through 0, 0 to 0, 4") {
+        lm5x5.turnOn(0, 0, 0, 4)
 
-        lm.lights(0) should contain only 1
+        lm5x5.lights(0) should contain only true
+    }
+
+    test("it should be light through 1, 1 to 2, 2") {
+        lm5x5.turnOn(1, 1, 2, 2)
+
+        lm5x5.lights(1)(1) shouldBe true
+        lm5x5.lights(1)(2) shouldBe true
+        lm5x5.lights(2)(1) shouldBe true
+        lm5x5.lights(2)(2) shouldBe true
+    }
+
+    test("it should not be light through 0,0 to 0, 4") {
+        lm5x5.turnOn(0, 0, 0, 4)
+
+        lm5x5.turnOff(0, 0, 0, 4)
+        lm5x5.lights(0) should contain only false
+    }
+
+    test("it should not be light through 1, 1 to 2, 2") {
+        lm5x5.turnOn(1, 1, 2, 2)
+
+        lm5x5.turnOff(1, 1, 2, 2)
+
+        lm5x5.lights(1)(1) shouldBe false
+        lm5x5.lights(1)(2) shouldBe false
+        lm5x5.lights(2)(1) shouldBe false
+        lm5x5.lights(2)(2) shouldBe false
+    }
+
+    test("it should toggle through 1,1 to 2,2") {
+        lm5x5.turnOn(0, 0, 4, 4)
+
+        lm5x5.toggle(1, 1, 2, 2)
+
+        lm5x5.lights(1)(1) shouldBe false
+        lm5x5.lights(1)(2) shouldBe false
+        lm5x5.lights(2)(1) shouldBe false
+        lm5x5.lights(2)(2) shouldBe false
     }
 }
