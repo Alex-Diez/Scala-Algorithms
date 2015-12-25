8f82b065f608ff2376d1dcde2db062b2d1c6dfa6
Alex-Diez <alexvic.amatory@gmail.com>
2015-12-25 14:35:41 +0000

    string code representation part 2

--- advent-code/src/main/scala/org/algorithms/CodeStringsMeter.scala
+++ advent-code/src/main/scala/org/algorithms/CodeStringsMeter.scala
@@ -1,13 +1,49 @@
 package org.algorithms
 
+import scala.annotation.tailrec
+import scala.io.Source._
+
 class CodeStringsMeter {
+    val escapedChars = Set("\\\'", "\\\"", "\\\\")
+
     def stringLength(codeString: String) = codeString.length
 
-    def codeStringLength(codeString: String) = {
-        var index = 1
-        while(index < codeString.length) {
-            
+    def codeStringLength(codeString: String): Int = {
+        @tailrec
+        def parseChar(string: String, index: Int, counter: Int): Int = {
+            if(index < string.length)
+                if(index + 2 < string.length && escapedChars.contains(string.substring(index, index + 2)))
+                    parseChar(string, index + 2, counter + 1)
+                else if(index + 2 < string.length && string.substring(index, index + 2) == "\\x")
+                         parseChar(string, index + 4, counter + 1)
+                else parseChar(string, index + 1, counter + 1)
+            else counter
         }
-        index - 1
+        parseChar(codeString, 0, 0) - 2
+    }
+
+    def escape(string: String): String = {
+        @tailrec
+        def escape(string: String, builder: StringBuilder, index: Int): String = {
+            if(index < string.length) {
+                val c = string.charAt(index)
+                if(c == '\"' || c == '\\' || c == '\'') {
+                    escape(string, builder.append('\\').append(c), index + 1)
+                }
+                else escape(string, builder.append(c), index + 1)
+            }
+            else builder.append('\"').toString
+        }
+        escape(string, new StringBuilder("\""), 0)
+    }
+}
+
+object CodeStringsMeter {
+
+    def main(args: Array[String]): Unit = {
+        val csm = new CodeStringsMeter
+        println(fromFile("advent-code/src/main/resources/code_strings").getLines.map[Int](s => csm.stringLength(s) - csm
+                .codeStringLength(s)).sum)
+        println(fromFile("advent-code/src/main/resources/code_strings").getLines.map[Int](s =>  csm.stringLength(csm.escape(s)) - csm.stringLength(s)).sum)
     }
 }
--- advent-code/src/test/scala/org/algorithms/CodeStringsMeterTest.scala
+++ advent-code/src/test/scala/org/algorithms/CodeStringsMeterTest.scala
@@ -4,20 +4,14 @@ import org.scalatest.{Matchers, FunSuite}
 
 class CodeStringsMeterTest extends FunSuite with Matchers {
 
-    test("create a string meter") {
-        new CodeStringsMeter
-    }
+    val csm = new CodeStringsMeter
 
     test("length of double quote is 2 and code length is 0") {
-        val csm = new CodeStringsMeter
-
         csm.stringLength("\"\"") shouldBe 2
         csm.codeStringLength("\"\"") shouldBe 0
     }
 
     test("strings with characters") {
-        val csm = new CodeStringsMeter
-
         csm.stringLength("\"abs\"") shouldBe 5
         csm.codeStringLength("\"abs\"") shouldBe 3
 
@@ -35,15 +29,38 @@ class CodeStringsMeterTest extends FunSuite with Matchers {
     }
 
     test("strings with escape symbols") {
-        val csm = new CodeStringsMeter
-
         csm.stringLength("\"\\\\\"") shouldBe 4
-        csm.codeStringLength("\"\\\\\"") shouldBe 3
+        csm.codeStringLength("\"\\\\\"") shouldBe 1
 
         csm.stringLength("\"\\\"\"") shouldBe 4
-        csm.codeStringLength("\"\\\"\"") shouldBe 3
+        csm.codeStringLength("\"\\\"\"") shouldBe 1
 
         csm.stringLength("\"\\\'\"") shouldBe 4
-        csm.codeStringLength("\"\\\'\"") shouldBe 3
+        csm.codeStringLength("\"\\\'\"") shouldBe 1
+    }
+
+    test("hex representation of char") {
+        csm.stringLength("\"v\\xfb\\\"lgs\\\"kvjfywmut\\x9cr\"") shouldBe 28
+        csm.codeStringLength("\"v\\xfb\\\"lgs\\\"kvjfywmut\\x9cr\"") shouldBe 18
+    }
+
+    test("escape strings") {
+        csm.escape("\"\"") shouldBe "\"\\\"\\\"\""
+
+        csm.escape("\"abc\"") shouldBe "\"\\\"abc\\\"\""
+
+        csm.escape("\"aaa\\\"aaa\"") shouldBe "\"\\\"aaa\\\\\\\"aaa\\\"\""
+
+        csm.escape("\"\\x27\"") shouldBe "\"\\\"\\\\x27\\\"\""
+    }
+
+    test("length of escape strings") {
+        csm.stringLength(csm.escape("\"\"")) shouldBe 6
+
+        csm.stringLength(csm.escape("\"abc\"")) shouldBe 9
+
+        csm.stringLength(csm.escape("\"aaa\\\"aaa\"")) shouldBe 16
+
+        csm.stringLength(csm.escape("\"\\x27\"")) shouldBe 11
     }
 }
